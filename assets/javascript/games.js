bf08c14929674476dcea363e43e0e0e38be28d10
Kevin Wu <peiyuwu9@gmail.com>
2019-09-25 15:41:45 -0400

    Change Game Over to alert. Change order in javascript.

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -2,6 +2,7 @@ var guessChances = 10;
 var guessLetter = [];
 var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var wins = 0;
+var losses =0;
 
 document.onkeyup = function (event) {
 
@@ -10,32 +11,56 @@ document.onkeyup = function (event) {
     var userGuess = event.key.toLocaleLowerCase();
 
     console.log (userGuess);
-
-    if ((userGuess==="a")||(userGuess==="b")||(userGuess==="c")||(userGuess==="d")||(userGuess==="e")||(userGuess==="f")||(userGuess==="g")||(userGuess==="h")||(userGuess==="i")||(userGuess==="j")||(userGuess==="k")||(userGuess==="l")||(userGuess==="m")||(userGuess==="n")||(userGuess==="o")||(userGuess==="p")||(userGuess==="q")||(userGuess==="r")||(userGuess==="s")||(userGuess==="t")||(userGuess==="u")||(userGuess==="v")||(userGuess==="w")||(userGuess==="x")||(userGuess==="y")||(userGuess==="z")) {
         
         if (guessChances > 0) {
             
             guessLetter.push(userGuess);
             
             guessChances--;
-            
-            var computerGuess = computerChoices [Math.floor(Math.random() * computerChoices.length)];
-            
-            console.log (computerGuess);
 
-            if (userGuess === computerGuess) {wins++};
+            if ((userGuess==="a")||(userGuess==="b")||(userGuess==="c")||(userGuess==="d")||(userGuess==="e")||(userGuess==="f")||(userGuess==="g")||(userGuess==="h")||(userGuess==="i")||(userGuess==="j")||(userGuess==="k")||(userGuess==="l")||(userGuess==="m")||(userGuess==="n")||(userGuess==="o")||(userGuess==="p")||(userGuess==="q")||(userGuess==="r")||(userGuess==="s")||(userGuess==="t")||(userGuess==="u")||(userGuess==="v")||(userGuess==="w")||(userGuess==="x")||(userGuess==="y")||(userGuess==="z")) {
+            
+                var computerGuess = computerChoices [Math.floor(Math.random() * computerChoices.length)];
+                
+                console.log (computerGuess);
 
-            document.getElementById("letters-text").textContent = "You guessed " + guessLetter;
-            document.getElementById("chances-text").textContent = "You have " + guessChances + " chances left.";
-            document.getElementById("wins-text").textContent = "You win " + wins + " times.";
+                if (userGuess === computerGuess) {wins++};
+
+                document.getElementById("letters-text").textContent = "You guessed: " + guessLetter;
+                document.getElementById("chances-text").textContent = "You have " + guessChances + " chances left.";
+                document.getElementById("wins-text").textContent = "You win " + wins + " times.";
+                document.getElementById("losses-text").textContent = "You loss " + losses + " times.";
+            }
+
+            else {
+                alert ("Please press letter key!");
+            }
         }
 
         else {
-            document.getElementById("game-over").textContent = "Game Over!!";
-        }
-    }
+            var resetGame = function () {
+                guessChances = 10;
+                wins = 0;
+                guessLetter = [];
+                
+                document.getElementById("letters-text").textContent = "You guessed: " + guessLetter;
+                document.getElementById("chances-text").textContent = "You have " + guessChances + " chances left.";
+                document.getElementById("wins-text").textContent = "You win " + wins + " times.";
+                document.getElementById("losses-text").textContent = "You loss " + losses + " times.";
+            }
 
-    else {
-        alert ("Please press letter key!");
-    }
+            if (losses < 2) {
+                var restart = confirm ("Do you want to play again?");
+                losses ++;
+                resetGame();
+            }
+
+            else {
+                alert ("Game Over!");
+                losses = 0;
+                resetGame();
+             }
+                    
+        }
+    
 }
