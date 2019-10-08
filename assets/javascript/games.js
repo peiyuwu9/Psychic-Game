6e0135e41aba83597b727e56decc75ffe854f01b
Kevin Wu <peiyuwu9@gmail.com>
2019-10-08 00:04:53 -0400

    Create the game reset function and repeat keying function

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,8 +1,30 @@
-var guessChances = 10;
+var guessChances = 13;
 var guessLetter = [];
 var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 var wins = 0;
-var losses =0;
+var losses = 0;
+var computerGuess;
+
+var resetGame = function() {
+    guessChances = 13;
+    guessLetter = [];
+    log();
+    randomNumberGenerate();
+}
+
+var log = function() {
+    document.getElementById("letters-text").textContent = "You guessed: " + guessLetter;
+    document.getElementById("chances-text").textContent = "You have " + guessChances + " chances left.";
+    document.getElementById("wins-text").textContent = "You win " + wins + " times.";
+    document.getElementById("losses-text").textContent = "You loss " + losses + " times.";
+}
+
+var randomNumberGenerate = function() {
+    computerGuess = computerChoices [Math.floor(Math.random() * computerChoices.length)];
+    console.log (computerGuess);
+}
+
+randomNumberGenerate();
 
 document.onkeyup = function (event) {
 
@@ -13,51 +35,43 @@ document.onkeyup = function (event) {
     console.log (userGuess);
         
         if (guessChances > 0) {
-            
-            guessLetter.push(userGuess);
-            
-            guessChances--;
 
-            if ((userGuess==="a")||(userGuess==="b")||(userGuess==="c")||(userGuess==="d")||(userGuess==="e")||(userGuess==="f")||(userGuess==="g")||(userGuess==="h")||(userGuess==="i")||(userGuess==="j")||(userGuess==="k")||(userGuess==="l")||(userGuess==="m")||(userGuess==="n")||(userGuess==="o")||(userGuess==="p")||(userGuess==="q")||(userGuess==="r")||(userGuess==="s")||(userGuess==="t")||(userGuess==="u")||(userGuess==="v")||(userGuess==="w")||(userGuess==="x")||(userGuess==="y")||(userGuess==="z")) {
-            
-                var computerGuess = computerChoices [Math.floor(Math.random() * computerChoices.length)];
+            if (guessLetter.indexOf(userGuess) === -1) {
                 
-                console.log (computerGuess);
+                guessLetter.push(userGuess);
+            
+                guessChances--;
 
-                if (userGuess === computerGuess) {wins++};
+                log();
+            }
+            
+            if (event.which>64 && event.which <91) {
 
-                document.getElementById("letters-text").textContent = "You guessed: " + guessLetter;
-                document.getElementById("chances-text").textContent = "You have " + guessChances + " chances left.";
-                document.getElementById("wins-text").textContent = "You win " + wins + " times.";
-                document.getElementById("losses-text").textContent = "You loss " + losses + " times.";
+                if (userGuess === computerGuess) {
+                    wins++
+                    resetGame();
+                };
+
             }
 
             else {
                 alert ("Please press letter key!");
             }
+            
         }
 
         else {
-            var resetGame = function () {
-                guessChances = 10;
-                wins = 0;
-                guessLetter = [];
-                
-                document.getElementById("letters-text").textContent = "You guessed: " + guessLetter;
-                document.getElementById("chances-text").textContent = "You have " + guessChances + " chances left.";
-                document.getElementById("wins-text").textContent = "You win " + wins + " times.";
-                document.getElementById("losses-text").textContent = "You loss " + losses + " times.";
-            }
 
             if (losses < 2) {
-                var restart = confirm ("Do you want to play again?");
                 losses ++;
+                wins = 0;
                 resetGame();
             }
 
             else {
                 alert ("Game Over!");
                 losses = 0;
+                wins = 0;
                 resetGame();
              }
                     
